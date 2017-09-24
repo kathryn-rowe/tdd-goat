73d8332be0112c0e96819597eac53b96105ee87a
Kathryn Rowe <kjrowe06@gmail.com>
2017-09-24 15:38:47 -0700

    Through ch 4 in book

---
+++ goat_app/superlists/lists/tests.py
@@ -0,0 +1,12 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
