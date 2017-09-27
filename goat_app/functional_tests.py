a56fe566f64454139dfe49e1da060cba6344248b
Kathryn Rowe <kjrowe06@gmail.com>
2017-09-26 20:22:49 -0700

    Created DB

--- goat_app/functional_tests.py
+++ goat_app/functional_tests.py
@@ -16,6 +16,12 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.close()
 		display.stop()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertIn(row.text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Check the homepage
@@ -41,18 +47,19 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
-
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
